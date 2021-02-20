8e44d678b20082ef70c6746331e1407926972d67
unknowngold <unknowngoldpath@gmail.com>
2021-02-20 20:07:48 +0100

    updating master with last code done

---
+++ android/app/src/main/assets/public/static/js/5.5f60f6cc.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpquantic-react"]=this["webpackJsonpquantic-react"]||[]).push([[5],{296:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var c=e(32),i=(e(7),function(){var n=window;n.addEventListener("statusTap",(function(){Object(c.g)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var o=i.closest("ion-content");o&&o.componentOnReady().then((function(){Object(c.m)((function(){return o.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.5f60f6cc.chunk.js.map
